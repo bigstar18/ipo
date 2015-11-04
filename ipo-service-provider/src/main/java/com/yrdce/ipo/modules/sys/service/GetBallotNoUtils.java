eb0e50340bfa8744cc3d97858e7f4b86a1b72c28
zyg <zyg@LAPTOP-TPUB2AKJ>
2015-11-04 23:39:28 +0800

    摇号中签业务修改

--- ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/service/GetBallotNoUtils.java
+++ ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/service/GetBallotNoUtils.java
@@ -2,71 +2,252 @@ package com.yrdce.ipo.modules.sys.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import com.yrdce.ipo.modules.sys.entity.IpoOrder;
+import com.yrdce.ipo.modules.sys.dao.IpoBallotNoInfoMapper;
+import com.yrdce.ipo.modules.sys.dao.IpoCommodityMapper;
+import com.yrdce.ipo.modules.sys.dao.IpoDistributionMapper;
+import com.yrdce.ipo.modules.sys.dao.IpoOrderMapper;
+import com.yrdce.ipo.modules.sys.entity.IpoBallotNoInfo;
 import com.yrdce.ipo.modules.sys.entity.IpoCommodity;
+import com.yrdce.ipo.modules.sys.entity.IpoCommodityExample;
+import com.yrdce.ipo.modules.sys.entity.IpoDistribution;
+
 /**
  * 摇号处理类
+ * 
  * @author zyg
  *
  */
 public class GetBallotNoUtils {
-	
-    //执行摇号处理路基
-	public static void executeApplyBallotNo(){
-		//新股摇号规则
+
+	//配号业务处理
+	private static IpoDistributionMapper ipoDistrMapper;
+	//订单业务处理
+	private static IpoOrderMapper ipoOrderMapper;
+	//商品业务处理
+	private static IpoCommodityMapper ipoCommdityMapper;
+	//中签信息业务处理
+	private static IpoBallotNoInfoMapper ipoBallotNoInfoMapper;
+
+	// 执行摇号处理开始
+	public static void start() {
+		executeApplyBallotNo();
+	}
+
+	// 执行摇号处理执行方法
+	public static void executeApplyBallotNo() {
+		// 新股摇号规则
 		MathUtil util = new MathUtil();
-		//产品编号，中签数量，中签号码,商品对应的中签号码
-		Map<String, Map<Integer, String>> commdityCorreSuccessNo = new HashMap<String, Map<Integer, String>>();
-		//中签数量，中签号码
-		Map<Integer, String> correSuccessNo = new HashMap<Integer, String>();
-		//调用查询所有商品列表Service------------------------------------------------------//调用高波的查询方法
-		List<IpoCommodity>  ipoCommoditys = new ArrayList<IpoCommodity>();
-		//调用查询所有订单列表Service------------------------------------------------------//调用高波的查询方法
-		List<IpoOrder> ipoOrders = new ArrayList<IpoOrder>();
+		// 产品编号,中签号码,商品对应的中签号码
+		Map<String, String> commdityCorreSuccessNo = new HashMap<String, String>();
+		// 根据当前时间T-2调用查询所有商品列表Service------------------------------------------------------//调用高波的查询方法
+		List<IpoCommodity> ipoCommodityList =ipoCommdityMapper.selectByExample(new IpoCommodityExample());
+		// 根据当前时间T-2调用查询所有订单列表Service------------------------------------------------------//调用高波的查询方法
+		List<IpoOrder> ipoOrders = ipoOrderMapper.selectAll();
+		//查询订单表中的商品编号
+		List<String> commdityIdList = ipoOrderMapper.select();
 		
-		
-		//获取每个商品的中签率
-		Map<String,String> successRateMap = GetSuccessRate.getSuccessRateByApplyCount(ipoOrders, ipoCommoditys);
-		if(successRateMap != null){
-			for(String commdityId : successRateMap.keySet()){
+		//将订单中的商品编号存放到Map中
+		Map<String,String> commdityIdsMap = new HashMap<String,String>();
+        if(commdityIdList != null || commdityIdList.size()> 0){
+        	for(int i=0;i < commdityIdList.size();i++)
+        		commdityIdsMap.put(commdityIdList.get(i), commdityIdList.get(i));
+        }
+        //将订单里面的商品编号和商品表进行匹配
+        List<IpoCommodity> ipoCommoditys = new ArrayList<IpoCommodity>();
+        if(ipoCommodityList != null || ipoCommodityList.size() > 0){
+        	for(int j=0;j < ipoCommodityList.size();j++){
+        		IpoCommodity ipoCommdi = ipoCommodityList.get(j);
+        		if(commdityIdsMap.containsKey(ipoCommdi.getCommodityid())){
+        			ipoCommoditys.add(ipoCommdi);
+        		}
+        	}
+        }
+
+		// ========1. 获取每个商品的中签率=====
+		Map<String, String> successRateMap = GetSuccessRate.getSuccessRateByApplyCount(ipoOrders, ipoCommoditys);
+		if (successRateMap != null) {
+			for (String commdityId : successRateMap.keySet()) {
 				String successRateStr = successRateMap.get(commdityId);
-				//将中签率转换为double
+				// 将中签率转换为double
 				double successRate = Double.valueOf(successRateStr);
-				//根据中签率获取中签号码
+				// ========2. 根据中签率获取中签号码=========
 				List<String> result = util.getRandomResult(successRate, 0);
-				//中签数量
-				int successNoNum = result.size();
-				//中签号码字符串
-				String successNoStr ="";
-				if(result != null || result.size() > 0 ){
-					for(int i = 0;i < result.size(); i++){
-						successNoStr = result.get(i)+",";
+				// 中签号码字符串
+				String successNoStr = "";
+				if (result != null || result.size() > 0) {
+					for (int i = 0; i < result.size(); i++) {
+						successNoStr = result.get(i) + ",";
 					}
 				}
-				//按照产品编号，中签数量，中签号码,商品对应的中签号码顺序存放到Map中
-				correSuccessNo.put(successNoNum, successNoStr);
-				commdityCorreSuccessNo.put(commdityId, correSuccessNo);	
+				// 按照产品编号，中签号码,商品对应的中签号码顺序存放到Map中
+				commdityCorreSuccessNo.put(commdityId, successNoStr);
 			}
 		}
 
+		// -----------------------------------根据系统时间T-2获取配号信息（高波）
+		// 获取当前系统时间
+		Date systemTime = new Date();
+		List<IpoDistribution> ipoDistrList = ipoDistrMapper.selectAll();
+
+		// ==========3. 根据中签号码计算出用户商品对应的中签配号数量并且更新配号结果表===========
+		calculateCommdityOfUserSuccessNoCount(commdityCorreSuccessNo, ipoDistrList);
+
+		// ===========4. 取出商品编号，中签号码后三位数，中签起始位数，结束位数存放入列表=======
+		List<IpoBallotNoInfo> ipoBallotNoInfoList = new ArrayList<IpoBallotNoInfo>();//中签信息列表对象
+		if (commdityCorreSuccessNo != null) {
+			for (String commdityId : commdityCorreSuccessNo.keySet()) {
+				// 中签号码，格式“，”号隔开字符串
+				String ballotStr = commdityCorreSuccessNo.get(commdityId);
+				// 根据字符串截取中签号码末三位数,中签号码起始位数，结束位数集合
+				Map<String, Map<Integer, Integer>> ballotNoStartEndNumMap = GetSuccessRate
+						.getBallotNoEndThree(ballotStr);
+				// 调用插入IPO_BALLOTNO_INFO表的Dao里面的插入方法----------------------------------------------------------
+				// 将产品编号和中签后三位数入库
+				if (ballotNoStartEndNumMap != null) {
+					for (String ballotNoThree : ballotNoStartEndNumMap.keySet()) {
+						// ballotNoThree末三位数
+						Map<Integer, Integer> ballotStartEndNumMap = ballotNoStartEndNumMap.get(ballotNoThree);
+						//中签信息对象
+						IpoBallotNoInfo ipoBallotNoInfo = new IpoBallotNoInfo();
+						if (ballotStartEndNumMap != null) {
+							//给中签信息对象赋值
+							ipoBallotNoInfo.setCommodityId(commdityId);
+							ipoBallotNoInfo.setBallotNo(ballotNoThree);
+							ipoBallotNoInfo.setCreateTime(new Date());
+							if (ballotStartEndNumMap != null) {
+								for (Integer start : ballotStartEndNumMap.keySet()) {
+									ipoBallotNoInfo.setBallotNoStartLen(start);
+									ipoBallotNoInfo.setBallotNoStartLen(ballotStartEndNumMap.get(start));
+								}
+							}
+						}
+						ipoBallotNoInfoList.add(ipoBallotNoInfo);
+					}
+				}
+			}
+		}
 		
-		//取出中签号码后三位数字入库。
-		if(commdityCorreSuccessNo != null ){
-		  for(String key : commdityCorreSuccessNo.keySet()){
-			  Map<Integer,String> map = commdityCorreSuccessNo.get(key);
-			  Collection<String> allotNos = map.values();
-			  for(String ballotNoStr:allotNos){
-				  GetSuccessRate.getBallotNoEndThree(ballotNoStr);
-				  //调用插入IPO_BALLOTNO_INFO表的Dao里面的插入方法
-				  //1.将产品编号和中签后三位数入库
-				  
-			  }
-		  } 
+		// 5. =========将中签对象列表统一入库==========
+        if(ipoBallotNoInfoList != null || ipoBallotNoInfoList.size() > 0){
+        	for(int i =0;i < ipoBallotNoInfoList.size();i++){
+        		IpoBallotNoInfo ipobal = ipoBallotNoInfoList.get(i);
+        		//调用入库方法------------------------------高波提供
+        		ipoBallotNoInfoMapper.insert(ipobal);
+        	}
+        }
+		
+
+	}
+
+	/**
+	 * 根据中签号码计算出用户商品对应的中签号码数量
+	 * 
+	 * @param commdityCorreSuccessNo
+	 * @param ipoDistrList
+	 */
+	public static void calculateCommdityOfUserSuccessNoCount(Map<String, String> commdityCorreSuccessNo,
+			List<IpoDistribution> ipoDistrList) {
+		if (ipoDistrList != null || ipoDistrList.size() > 0) {
+			for (int i = 0; i < ipoDistrList.size(); i++) {
+				IpoDistribution ipoDis = ipoDistrList.get(i);
+				String commdityId = ipoDis.getCommodityid();
+				// 判断配号列表中产品与Map<商品编号，中签号码>中商品编号相同
+				if (commdityCorreSuccessNo.containsKey(commdityId)) {
+					// 获取中签号码字符串
+					String ballotNoStr = commdityCorreSuccessNo.get(commdityId);
+					// 取出Map中商品编号对应中签号码字符串
+					String[] ballotNos = ballotNoStr.split(",");
+					int g = 0;
+					while (g < ballotNos.length) {
+						String ballotNo = ballotNos[g];
+						// 根据每一位中签号码获取每个用户对应商品的中签数量
+						getBallotNoCount(ipoDistrList, ballotNo);
+						g++;
+					}
+				}
+			}
+
 		}
 
-		
+		// if (commdityCorreSuccessNo != null) {
+		// for (String key : commdityCorreSuccessNo.keySet()) {
+		// // 获取各个商品的中签号码
+		// String ballotStr = commdityCorreSuccessNo.get(key);
+		// String[] ballotNos = ballotStr.split(",");
+		// int g = 0;
+		// while (g < ballotNos.length) {
+		// String ballotNo = ballotNos[g];
+		// getBallotNoCount(ipoDistrList, ballotNo);
+		// g++;
+		// }
+		//
+		// }
+		// }
+
 	}
+
+	public static void getBallotNoCount(List<IpoDistribution> ipoDistrList, String ballotNo) {
+		// 形成一个新的配号列表
+		List<IpoDistribution> newIpoDistributionList = new ArrayList<IpoDistribution>();
+		if (ipoDistrList != null || ipoDistrList.size() > 0) {
+			for (int i = 0; i < ipoDistrList.size(); i++) {
+				IpoDistribution newIpoDis = new IpoDistribution();
+				IpoDistribution ipoDis = ipoDistrList.get(i);
+				// 用户编号
+				String userId = ipoDis.getUserid();
+				// 产品编号
+				String commdityId = ipoDis.getCommodityid();
+				// 配号起始号
+				long startNum = ipoDis.getStartnumber();
+				// 配号数量
+				int count = ipoDis.getPcounts();
+				// 配号时间
+				Date ipoTime = ipoDis.getPtime();
+				// 中签数量
+				int ipoDisZcounts = ipoDis.getZcounts();
+
+				// 取出对应商品所有配号
+				List distrNos = new ArrayList();
+				for (int j = 0; i < count; j++) {
+					distrNos.add(startNum + j);
+				}
+
+				// 用此用户生成的配号去和中签号码比对，查看中签的号码的数量
+				int f = 0;
+				while (f < distrNos.size()) {
+					// 获取每一个中签号码
+					String disNo = distrNos.get(f).toString();
+					String disNoDigit = disNo.substring(disNo.length() - ballotNo.length(), disNo.length());
+					if (ballotNo.equals(disNoDigit)) {
+						ipoDisZcounts = ipoDisZcounts + 1;
+					}
+					f++;
+				}
+
+				// 新配号列表只需要存储 用户编号，商品编号，中签数量
+				newIpoDis.setUserid(userId);
+				newIpoDis.setCommodityid(commdityId);
+				newIpoDis.setZcounts(ipoDisZcounts);
+				newIpoDistributionList.add(newIpoDis);
+
+			}
+
+			// 根据用户编号，商品编号作为更新条件去更新中签数量
+			if (newIpoDistributionList != null || newIpoDistributionList.size() > 0) {
+				for (int i = 0; i < newIpoDistributionList.size(); i++) {
+					IpoDistribution ipoDistribution = newIpoDistributionList.get(i);
+					// 调用更新方法----------------------//后期需添加，时间作为条件
+					ipoDistrMapper.updateBycomAndUserid(ipoDistribution);
+				}
+
+			}
+
+		}
+	}
+
 }
--- ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/service/GetSuccessRate.java
+++ ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/service/GetSuccessRate.java
@@ -7,35 +7,33 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-// import com.yrdce.ipo.common.utils.ThreadClassTemplet;
-import com.yrdce.ipo.modules.sys.entity.IpoCommodity;
+import com.yrdce.ipo.modules.sys.entity.IpoDistribution;
 import com.yrdce.ipo.modules.sys.entity.IpoOrder;
+import com.yrdce.ipo.modules.sys.entity.IpoCommodity;
 
 /**
  * 获取中签率具体方法
- * 
  * @author zyg
  *
  */
 public class GetSuccessRate {
 	public static Logger logger = Logger.getLogger(GetSuccessRate.class);
-
-	public static Map<String, String> getSuccessRateByApplyCount(List<IpoOrder> ipoOrders, List<IpoCommodity> ipoCommoditys) {
-		// 根据商品编号统计客户申购数量
+	public static Map<String,String>  getSuccessRateByApplyCount(List<IpoOrder> ipoOrders,List<IpoCommodity> ipoCommoditys){
+		// 根据商品编号统计客户申购数量 Map<商品编号,申购单位数>
 		Map<String, Integer> orderCountByCommodityMap = new HashMap<String, Integer>();
-		// 根据商品编号统计发行数量
+		// 根据商品编号统计发行数量 Map<商品编号,发行数量>
 		Map<String, Integer> disbutCountByCommodityMap = new HashMap<String, Integer>();
-		// 每个商品的中签率
-		Map<String, String> successRateMap = new HashMap<String, String>();
-
+        //每个商品的中签率 Map<商品编号，中签率>
+		Map<String,String> successRateMap = new HashMap<String,String>();
+		
 		// 统计每个商品被客户申购的总数量
 		// 统计申购单位数
 		int applyPurCount = 0;
 		if (ipoOrders != null || ipoOrders.size() > 0) {
 			for (int i = 0; i < ipoOrders.size(); i++) {
-				// 获取每个产品信息
+				//获取每个产品信息
 				IpoOrder ipoOrder = ipoOrders.get(i);
-				// 判断当前产品在Map中是否存在，如果存在就将申购数量累加，如果不存在就直接存放Map中
+				//判断当前产品在Map中是否存在，如果存在就将申购数量累加，如果不存在就直接存放Map中
 				if (orderCountByCommodityMap.containsKey(ipoOrder.getCommodityid())) {
 					applyPurCount = ipoOrder.getCounts();
 					int count = orderCountByCommodityMap.get(ipoOrder.getCommodityid());
@@ -50,106 +48,101 @@ public class GetSuccessRate {
 		// 每个商品发布的总数量统计
 		if (ipoCommoditys != null || ipoCommoditys.size() > 0) {
 			for (int i = 0; i < ipoCommoditys.size(); i++) {
-				// 获取每个产品信息
+				//获取每个产品信息
 				IpoCommodity ipoCommodity = ipoCommoditys.get(i);
-				// 判断当前产品在Map中是否存在，如果不存在就将商品编号和发布数量存放
+				//判断当前产品在Map中是否存在，如果不存在就将商品编号和发布数量存放
 				if (!disbutCountByCommodityMap.containsKey(ipoCommodity.getCommodityid())) {
 					disbutCountByCommodityMap.put(ipoCommodity.getCommodityid(), ipoCommodity.getCounts());
-				} else {
+				}else{
 					logger.info("发布的商品有重复！");
-				}
+				} 
 			}
 		}
 
-		// 计算出每个商品的中签率
+		// 计算出每个商品的中签率 
 		if (disbutCountByCommodityMap != null) {
-			// 获取发布商品信息Map的KEY值集合
+			//获取发布商品信息Map的KEY值集合
 			for (String orderkey : disbutCountByCommodityMap.keySet()) {
-				// 根据商品编号获取发布数量
+				//根据商品编号获取发布数量
 				int disCount = disbutCountByCommodityMap.get(orderkey);
-				// 根据商品编号判断申购MAP中是否存在此商品
-				if (orderCountByCommodityMap.containsKey(orderkey)) {
-					// 根据商品编号获取申购的单位数
-					int purCount = orderCountByCommodityMap.get(orderkey);
-					// 中签率=股票发行股数/有效申购股数*100
-					double successRate = (double) disCount / (double) (purCount * 1000);
-					// 将中签率转换为String类型存放到Map中
-					String successRateStr = String.valueOf(successRate);
-					successRateMap.put(orderkey, successRateStr);
-				} else {
-					logger.info("申购表中没有此产品的申够记录！");
-				}
+				//根据商品编号判断申购MAP中是否存在此商品
+               if(orderCountByCommodityMap.containsKey(orderkey)){
+            	   //根据商品编号获取申购的单位数
+            	   int purCount = orderCountByCommodityMap.get(orderkey);
+            	   //中签率=股票发行股数/有效申购股数*100
+            	   double successRate = (double)disCount/(double)(purCount*1000);
+            	   //将中签率转换为String类型存放到Map中
+            	   String successRateStr = String.valueOf(successRate);
+            	   successRateMap.put(orderkey, successRateStr);
+               }else{
+            	   logger.info("申购表中没有此产品的申够记录！");
+               }
 			}
 		}
-
+        
 		return successRateMap;
 	}
-
+	
 	/**
 	 * 根据产生中签号码位数（长度）计算出中签起始于结束位数
-	 * 
 	 * @param length
 	 * @return
 	 */
-	public static Map<Integer, Integer> getBallotNoStartLength(int length) {
-		Map<Integer, Integer> ballotNoLength = new HashMap<Integer, Integer>();
-		// 长度必须大于0
-		if (length > 0) {
-			if (length == 1) {
-				ballotNoLength.put(0, 8);
-			} else if (length == 2) {
+	public static Map<Integer,Integer> getBallotNoStartLength(String ballotNo){
+		int length = ballotNo.length();
+		Map<Integer,Integer> ballotNoLength = new HashMap<Integer,Integer>();
+		//长度必须大于0
+		if(length > 0){
+			if(length == 1){
+				ballotNoLength.put(8, 8);
+			}else if(length == 2){
 				ballotNoLength.put(7, 8);
-			} else if (length == 3) {
+			}else if(length == 3){
 				ballotNoLength.put(6, 8);
-			} else if (length == 4) {
+			}else if(length == 4){
 				ballotNoLength.put(5, 8);
-			} else if (length == 5) {
+			}else if(length == 5){
 				ballotNoLength.put(4, 8);
-			} else if (length == 6) {
+			}else if(length == 6){
 				ballotNoLength.put(3, 8);
-			} else if (length == 7) {
+			}else if(length == 7){
 				ballotNoLength.put(2, 8);
-			} else if (length == 8) {
+			}else if(length == 8){
 				ballotNoLength.put(1, 8);
 			}
-		} else {
+		}else{
 			logger.info("产生的中签号码不符合规则！");
 		}
-		return null;
+		return ballotNoLength;
 	}
-
+	
 	/**
-	 * 根据字符串截取中签号码末三位数
-	 * 
+	 * 根据字符串截取中签号码末三位数和起始位数于结束位数
 	 * @param ballotNosStr
 	 * @return
 	 */
-	public static String getBallotNoEndThree(String ballotNosStr) {
-		String ballotNoEndThree = "";
-		List<String> ballotStrList = new ArrayList<String>();
-		if (ballotNosStr != null || "".equals(ballotNosStr)) {
-			String[] strList = ballotNosStr.split(",");
-			for (int i = 0; i < strList.length; i++) {
-				String ballotNo = strList[i];
-				System.out.println("aaa:" + ballotNo);
-				if (ballotNo.length() > 3) {
-					String str1 = ballotNo.substring(ballotNo.length() - 3, ballotNo.length());
-					ballotStrList.add(str1);
-
-				} else if (strList.length == 3) {
-					ballotStrList.add(ballotNo);
-				} else {
-					String str3 = ballotNo;
-					ballotStrList.add(str3);
+	public static Map<String,Map<Integer,Integer>> getBallotNoEndThree(String ballotNosStr){
+		Map<String,Map<Integer,Integer>>  ballotStartAndEndNum = new HashMap<String,Map<Integer,Integer>>();
+		if(ballotNosStr != null || "".equals(ballotNosStr)){
+			//根据“，”号截取每一位中签号码
+			String[] strList =  ballotNosStr.split(",");
+			for(int i = 0;i < strList.length; i++){
+				//获取中签号码
+				String  ballotNo = strList[i];
+				//根据中签号码获取起始和结束位数
+				Map<Integer,Integer> ballotStartEndNum = getBallotNoStartLength(ballotNo);
+				
+				String ballotNoStr = "";
+				if(ballotNo.length() > 3){
+					ballotNoStr = ballotNo.substring(ballotNo.length()-3,ballotNo.length());
+				}else if(strList.length == 3){
+					ballotNoStr = ballotNo;
+				}else{
+					ballotNoStr =ballotNo;
 				}
+				ballotStartAndEndNum.put(ballotNoStr, ballotStartEndNum);
 			}
 		}
-		// 平凑字符串
-		if (ballotStrList != null || ballotStrList.size() > 0) {
-			for (int i = 0; i < ballotStrList.size(); i++) {
-				ballotNoEndThree += ballotStrList.get(i);
-			}
-		}
-		return ballotNoEndThree;
+		return ballotStartAndEndNum;
 	}
 }
