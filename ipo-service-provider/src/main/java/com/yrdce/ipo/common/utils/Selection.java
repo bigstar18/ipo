4497a6f6465e9b4a694ca58e7f2009b64564f175
li <li@LAPTOP-H4QBD0D0>
2015-11-26 20:41:29 +0800

    摇号 配号

---
+++ ipo-service-provider/src/main/java/com/yrdce/ipo/common/utils/Selection.java
@@ -0,0 +1,230 @@
+package com.yrdce.ipo.common.utils;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Selection {
+	private double iopNum;// 持仓量
+	private double buyNum;// 申购量
+	private String sucRate;// 中签率
+	private String decimalNum;
+	long tmp = 0l;
+	private List<String> endNumList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Selection s = new Selection();
+		int test=0;
+		List<String> temp = s.MainSelection(233450, 1005700);
+		for (int i = 0; i < temp.size(); i++) {
+			System.out.println(temp.get(i));
+			test+=s.OwnMatchingEndNum(10000001,5700,temp.get(i));
+			test+=s.OwnMatchingEndNum(10005700,500000,temp.get(i));
+			test+=s.OwnMatchingEndNum(10505700,100000,temp.get(i));
+			test+=s.OwnMatchingEndNum(10605700,400000,temp.get(i));
+			
+		}
+		System.out.println("尾号个数" + temp.size());
+		System.out.println("可匹配的个数" + s.tmp);
+		System.out.println("可匹配的个数" + test);
+
+	}
+
+	//对外开放主接口
+	public  List<String> MainSelection(double ipoNum, double buyNum) {
+		this.iopNum = ipoNum;
+		this.buyNum = buyNum;
+		GetSucRate();
+		SplitSucRate();
+		MatchingEndNum(endNumList);
+		AdjustmentNum();
+		return endNumList;
+	}
+	//总体中签数匹配
+	private void MatchingEndNum(List<String> temp){
+		for (int i = 0; i < temp.size(); i++) {
+			String endNum = temp.get(i);
+			int len = endNum.length();
+			String buyNumString = String.valueOf((int)buyNum);
+			if (Long.parseLong(buyNumString.substring(buyNumString.length() - len, buyNumString.length())) >= Long.parseLong(endNum)) {
+				if (buyNumString.length() == len)
+					tmp += 1;
+				else
+					tmp += Long.parseLong(buyNumString.substring(0, buyNumString.length() - len)) + 1l;
+			} else {
+				if (buyNumString.length() == len)
+					tmp += 1;
+				else
+					tmp += Long.parseLong(buyNumString.substring(0, buyNumString.length() - len));
+			}
+		}
+	}
+	//个人中签数匹配
+	public int OwnMatchingEndNum(int numStart,int numCounts,String numEnd){
+		String strNumStart = String.valueOf(numStart);
+		if(strNumStart.length()<numEnd.length()){
+			return 0;
+		}
+		int tempEnd = numStart+numCounts-1;
+		int iNumEnd = Integer.parseInt(numEnd);
+		String strTempEnd = String.valueOf(tempEnd);
+		String numStartEnd = strNumStart.substring(strNumStart.length()-numEnd.length(),strNumStart.length());
+		int lenEndNum = numEnd.length();
+		strNumStart = strNumStart.substring(0, strNumStart.length()-lenEndNum);
+		strTempEnd = strTempEnd.substring(0, strTempEnd.length()-lenEndNum);
+		int tempNumStart = Integer.parseInt(strNumStart);
+		int tempNumEnd = Integer.parseInt(strTempEnd);
+		if(tempNumEnd>tempNumStart){
+			if(IsContainRight(tempEnd,numEnd)&&IsContainLeft(numStart,numEnd)){
+				return tempNumEnd - tempNumStart+1;
+			}
+			else if(IsContainRight(tempEnd,numEnd)||IsContainLeft(numStart,numEnd)){
+					return tempNumEnd - tempNumStart;
+			}
+			else{ 
+					return tempNumEnd - tempNumStart-1;
+			}
+			
+		}else if(tempNumEnd == tempNumStart){
+			if(Integer.parseInt(numStartEnd)<=iNumEnd&&Integer.parseInt(numStartEnd)+numCounts-1>=iNumEnd){
+				return 1;
+			}else{
+				return 0;
+			}
+		}else{
+			return 0;
+		}
+	}
+	//判断尾号开始是否在区间内
+	private boolean IsContainLeft(int num,String numEnd){
+		String strNum = String.valueOf(num);
+		int iNumEnd = Integer.parseInt(numEnd);
+		strNum = strNum.substring(strNum.length()-numEnd.length(),strNum.length());
+		if(Integer.parseInt(strNum)<=iNumEnd){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//判断尾号结束是否在区间内
+		private boolean IsContainRight(int num,String numEnd){
+			String strNum = String.valueOf(num);
+			int iNumEnd = Integer.parseInt(numEnd);
+			strNum = strNum.substring(strNum.length()-numEnd.length(),strNum.length());
+			if(Integer.parseInt(strNum)>=iNumEnd){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	
+	// 号码补齐
+	private void AdjustmentNum() {
+		while (tmp < iopNum) {
+			int temp = (int)Math.pow(10, String.valueOf((int)buyNum).length()-1);
+			String zero = String.valueOf(temp).substring(1, String.valueOf(temp).length());
+			DecimalFormat df = new DecimalFormat(zero);
+			Random rd = new Random();
+			int iTemp = Integer.parseInt(String.valueOf((int) buyNum).substring(1, String.valueOf((int) buyNum).length()));
+			String strEndNum = String.valueOf(rd.nextInt(((int) buyNum - iTemp)) + iTemp);
+			while (!DeleSame(strEndNum)) {
+				strEndNum = String.valueOf(rd.nextInt(((int) buyNum - iTemp)) + iTemp);
+			}
+			if(strEndNum.length()< String.valueOf((int)buyNum).length()-1)
+				strEndNum = df.format(Integer.parseInt(strEndNum));
+			endNumList.add(strEndNum);
+			tmp++;
+		}
+	}
+
+	// 获取中签率
+	private void GetSucRate() {
+		double result = iopNum / buyNum;
+		int iTemp = String.valueOf((int) buyNum).length();
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(iTemp);
+		df.setGroupingSize(0);
+		df.setRoundingMode(RoundingMode.FLOOR);
+		sucRate = df.format(result);
+		String[] a_splitSucRate = sucRate.split("\\.");
+		decimalNum = a_splitSucRate[1];
+	}
+
+	// 拆分中签率
+	private void SplitSucRate() {
+		if(String.valueOf((int) buyNum).length() - 2>decimalNum.length()){
+			for (int i = 0; i < decimalNum.length(); i++) {
+				SplitSucRate(i);
+			}
+		}else{
+			for (int i = 0; i < String.valueOf((int) buyNum).length() - 2; i++) {
+				SplitSucRate(i);
+			}
+		}
+	}
+	
+	private void SplitSucRate(int i){
+		String m_Decimal = decimalNum.substring(i, i + 1);
+		if (!m_Decimal.equals("0")) {
+			int m_num = (int) Math.pow(10, i + 1);
+			GetOtherEndNum(Integer.parseInt(m_Decimal), m_num);
+		}
+	}
+
+	// 尾号查重
+	private boolean DeleSame(String strRd) {
+		if (endNumList.size() != 0) {
+			for (int i = 0; i < endNumList.size(); i++) {
+				if (strRd.endsWith(endNumList.get(i))) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			
+			return true;
+		}
+	}
+	
+	// 生成尾号
+	private void GetOtherEndNum(int m_num, int temp) {
+		if (temp % m_num == 0) {
+			Random rd = new Random();
+			int m_result = rd.nextInt(temp);
+			
+			String zero = String.valueOf(temp).substring(1, String.valueOf(temp).length());
+			DecimalFormat df = new DecimalFormat(zero);
+			while (!DeleSame(String.valueOf(m_result))) {
+				m_result = rd.nextInt(temp);
+			}
+			for (int i = 0; i < m_num; i++) {
+				int endNum = (m_result + i * (temp / m_num)) % temp;
+				if(endNum==0){
+					endNum++;
+				}
+				if (!endNumList.contains(df.format(endNum))) {
+					endNumList.add(df.format(endNum));
+				}
+			}
+		} else {
+			fnSplitNum(m_num, temp);
+		}
+	}
+
+	//中签率拆分后 不整除 中签率拆分
+	private void fnSplitNum(int m_num, int temp) {
+		if (m_num == 3) {
+			GetOtherEndNum(1, temp);
+			GetOtherEndNum(2, temp);
+		} else if (m_num == 4) {
+			GetOtherEndNum(2, temp);
+			GetOtherEndNum(2, temp);
+		} else if (m_num > 5) {
+			GetOtherEndNum(5, temp);
+			GetOtherEndNum(m_num % 5, temp);
+		}
+	}
+
+}
