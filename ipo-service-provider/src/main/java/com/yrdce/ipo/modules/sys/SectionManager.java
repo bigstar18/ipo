fe25fddc3d5be044dc055adaf11385bf0e541aed
hxx <hxx@hxx-PC>
2015-12-01 11:07:03 +0800

    mock section time

--- ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/SectionManager.java
+++ ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/SectionManager.java
@@ -3,6 +3,8 @@
  */
 package com.yrdce.ipo.modules.sys;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,5 +19,24 @@ public class SectionManager {
 
 	public void init() {
 
+	}
+
+	public boolean isOpenMarketTime(Date date) {
+		return true;// TODO
+	}
+
+	public long getOpenMarketTimeFromNow(Date date) {
+
+		return 100000l;// TODO
+	}
+
+	public long getTradeTimeFromNow(Date date) {
+
+		return 100000l;// TODO
+	}
+
+	public int getCurrentSectionId(Date date) {
+
+		return 1;// TODO
 	}
 }
