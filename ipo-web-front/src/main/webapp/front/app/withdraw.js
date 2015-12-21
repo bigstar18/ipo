03980cc9a8c353787a17bfadd44ad4166909ec60
huanglei <huangl@yrdce.com>
2015-12-21 16:52:15 +0800

    打印弹出页面

--- ipo-web-front/src/main/webapp/front/app/withdraw.js
+++ ipo-web-front/src/main/webapp/front/app/withdraw.js
@@ -4,9 +4,11 @@ $(function() {
     var value = $(".pickup").find("option:selected").val();
 
     if (value == 'customer') {
+    	$('.customer').removeClass('hide');
       $('.dispatching').addClass('hide');
     }
     if (value == 'dispatching') {
+    	$('.customer').addClass('hide');
       $('.dispatching').removeClass('hide');
     }
 
