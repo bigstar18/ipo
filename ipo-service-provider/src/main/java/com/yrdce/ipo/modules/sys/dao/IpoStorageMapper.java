f3cedbdcb8d939435a00d3ea510d96451920c951
chenjing <1036847269@qq.com>
2015-12-21 14:03:51 +0800

    IPOSTORAGEMAPPER annotation

--- ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/dao/IpoStorageMapper.java
+++ ipo-service-provider/src/main/java/com/yrdce/ipo/modules/sys/dao/IpoStorageMapper.java
@@ -3,8 +3,10 @@ package com.yrdce.ipo.modules.sys.dao;
 import java.util.Date;
 import java.util.List;
 
+import com.yrdce.ipo.common.dao.MyBatisDao;
 import com.yrdce.ipo.modules.sys.entity.IpoStorage;
 
+@MyBatisDao
 public interface IpoStorageMapper {
 	int deleteByPrimaryKey(String storageid);
 
@@ -12,12 +14,12 @@ public interface IpoStorageMapper {
 
 	IpoStorage selectByPrimaryKey(String storageid);
 
-    IpoStorage selectByCommodityId(String commodityid);
-    
-    IpoStorage selectByStorageDate(Date storagedate);
-    
-    IpoStorage selectByStorageState(int storagestate);
-	
+	IpoStorage selectByCommodityId(String commodityid);
+
+	IpoStorage selectByStorageDate(Date storagedate);
+
+	IpoStorage selectByStorageState(int storagestate);
+
 	List<IpoStorage> selectAll();
 
 	int updateByPrimaryKey(IpoStorage record);
