581d0942d5b9e32953d37a625ce5995dd8f8f9c6
黄磊 <JackyBird@huangleideMacBook-Pro.local>
2015-12-22 19:55:43 +0800

    提货申请测试

--- ipo-web-front/src/main/webapp/front/app/delivery/withdraw.js
+++ ipo-web-front/src/main/webapp/front/app/delivery/withdraw.js
@@ -15,4 +15,28 @@ $(function() {
   });
 
 
+  var withdraw = new Vue({
+    el: '#withdraw',
+    data: {
+      selected: '201510010000-50',
+      options: [
+        { text: '春树秋香图', value: '201510010000-50'},
+        { text: '梁非凡', value: '201510010020-55'},
+        { text: '刘醒', value: '201510010011-500'},
+        { text: '非凡哥', value: '201510010005-150'}
+      ]
+    }
+  });
+  // $.ajax({
+  //   type:"GET",
+  //   url:'http://172.17.12.23',
+  //   success: function(response) {
+  //     purchase.purchases = response;
+  //   },
+  //   error: function(response) {
+  //   }
+  // });
+  // console.log($('#vcode').val().split('-')[0]);
+  // var vcode = $('#vcode').val().split('-');
+  // $('#vcode').attr('value', vcode[0]);
 })
