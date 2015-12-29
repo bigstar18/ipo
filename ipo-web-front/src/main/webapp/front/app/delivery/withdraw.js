8b67207a1889cbfa91610b7e733cf28151fb288c
黄磊 <JackyBird@huangleideMacBook-Pro.local>
2015-12-29 17:06:14 +0800

    提货申请重新修改加载格式

--- ipo-web-front/src/main/webapp/front/app/delivery/withdraw.js
+++ ipo-web-front/src/main/webapp/front/app/delivery/withdraw.js
@@ -4,33 +4,33 @@ $(function() {
     var value = $(".pickup").find("option:selected").val();
 
     if (value == 'customer') {
-    	$('.customer').removeClass('hide');
+      $('.customer').removeClass('hide');
       $('.dispatching').addClass('hide');
     }
     if (value == 'dispatching') {
-    	$('.customer').addClass('hide');
+      $('.customer').addClass('hide');
       $('.dispatching').removeClass('hide');
     }
 
   });
 
 
-  var withdraw = new Vue({
-    el: '#withdraw',
-    data: {
-      selected: '201510010000-50',
-      options: [
-        { text: '春树秋香图', value: '201510010000-50'},
-        { text: '梁非凡', value: '201510010020-55'},
-        { text: '刘醒', value: '201510010011-500'},
-        { text: '非凡哥', value: '201510010005-150'}
-      ],
-      warehouseNames: [
-        {warehouseName: '长三角'},
-        {warehouseName: '东北亚'}
-      ]
-    }
-  });
+  var commodities = [{
+        "commodityname": '春树秋香图',
+        "commodityid": '201510010000',
+        "position": "50",
+        "warehouseName": ["1号仓库", "长三角"]
+      },{
+        "commodityname": '奔马图',
+        "commodityid": '11010000',
+        "position": "100",
+        "warehouseName": ["5号仓库", "东北亚", "长三角"]
+      },{
+        "commodityname": '墨竹戒指',
+        "commodityid": '666666',
+        "position": "2333",
+        "warehouseName": ["老王家", "长三角"]
+      }];
   // $.ajax({
   //   type:"GET",
   //   url:'http://172.17.12.23',
@@ -40,27 +40,76 @@ $(function() {
   //   error: function(response) {
   //   }
   // });
-  var vcode = $('#vcode').val().split('-');
-  $('#vcode').attr('value', vcode[0]);
-  $('#vcount').attr('value', vcode[1]);
-  $("#nametext").bind("change",function(){
-    if($(this).val()==0){
-      return;
+
+  //页面初始化
+  initial ();
+  //加载藏品名称数据
+  function initial () {
+
+    var selname = $('#nametext');
+    var selhouse = $('#housetext');
+    for(var i = 0; i < commodities.length; i++){
+      selname.append('<option logix =' + i + '>'+commodities[i].commodityname+'</option>');
     }
-    else{
-      var vcode = $('#vcode').val().split('-');
-      $('#vcode').attr('value', vcode[0]);
-      $('#vcount').attr('value', vcode[1]);
+    $("#vcode").val(commodities[0].commodityid);
+    $("#vcount").val(commodities[0].position);
+    var warehouses = commodities[0].warehouseName;
+    for(var i = 0; i < warehouses.length; i++){
+      selhouse.append('<option logix =' + i + '>'+warehouses[i]+'</option>');
+    }
+  }
+  //选择其他商品名称是变化
+  $('#nametext').change(function() {
+    var logic = $("#nametext").find("option:selected").attr('logix');
+
+    var selname = $('#nametext');
+    var selhouse = $('#housetext');
+    $("#vcode").val(commodities[logic].commodityid);
+    $("#vcount").val(commodities[logic].position);
+    var warehouses = commodities[logic].warehouseName;
+    selhouse.empty()
+    for(var i = 0; i < warehouses.length; i++){
+      selhouse.append('<option logix =' + i + '>'+warehouses[i]+'</option>');
     }
   });
-
-
-
-
-  // console.log(withdraw.options);
-  // console.log($('#vcode').val().split('-')[0]);
-  // console.log($('#vcode').val().split('-')[1]);
-  // var vcode = $('#vcode').val().split('-');
-  // $('#vcode').attr('value', vcode[0]);
-  // $('#vcount').attr('value', vcode[1]);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
