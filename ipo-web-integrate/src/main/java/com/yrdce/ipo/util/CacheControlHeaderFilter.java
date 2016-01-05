a1757623124ec9fac1ccafe82f106399584a1290
hxx <hxx@hxx-PC>
2016-01-06 01:05:15 +0800

    add hxx modify

--- ipo-web-integrate/src/main/java/com/yrdce/ipo/util/CacheControlHeaderFilter.java
+++ ipo-web-integrate/src/main/java/com/yrdce/ipo/util/CacheControlHeaderFilter.java
@@ -44,7 +44,7 @@ public class CacheControlHeaderFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		if (expiresSeconds == 0)
+		if (expiresSeconds == 0) // hxx
 			Servlets.setNoCacheHeader((HttpServletResponse) res);
 		else
 			Servlets.setExpiresHeader((HttpServletResponse) res, expiresSeconds);
