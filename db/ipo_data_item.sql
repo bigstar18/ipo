c497ee618a4ed5491786caf95e52f52be965a4e0
wq <wuqi@yrdce.com>
2016-01-16 22:22:28 +0800

    文档

--- db/ipo_data_item.sql
+++ db/ipo_data_item.sql
@@ -1,23 +1,26 @@
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('1', '承销会员', 'charge.business', '业务', 0);
+values ('001', '承销业务', 'charge.business', '业务', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('2', '发行会员', 'charge.business', '业务', 0);
+values ('002', '发行业务', 'charge.business', '业务', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('3', '经济会员', 'charge.business', '业务', 0);
+values ('003', '经济业务', 'charge.business', '业务', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('1', '市场', 'charge.role', '费用角色', 0);
+values ('001', '市场', 'charge.role', '费用角色', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('2', '发行商', 'charge.role', '费用角色', 0);
+values ('002', '发行商', 'charge.role', '费用角色', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('3', '承销商', 'charge.role', '费用角色', 0);
+values ('003', '承销商', 'charge.role', '费用角色', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('4', '经济会员', 'charge.role', '费用角色', 0);
+values ('004', '经济会员', 'charge.role', '费用角色', 0);
 
 insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
-values ('5', '交易商', 'charge.role', '费用角色', 0);
+values ('005', '交易商', 'charge.role', '费用角色', 0);
+
+insert into ipo_data_item (CODE, VALUE, TYPE, REMARK, DELETE_FLAG)
+values ('004', '增发业务', 'charge.business', '业务', 0);
