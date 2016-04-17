f8272f248b9b27a4606b045afecac05623f822e8
hxx <hxx@hxx-PC>
2016-04-17 19:24:50 +0800

    查询rootmenu sql语句错误

--- ipo-web-integrate/src/main/java/com/yrdce/ipo/integrate/QueryDao.java
+++ ipo-web-integrate/src/main/java/com/yrdce/ipo/integrate/QueryDao.java
@@ -1,13 +1,5 @@
 package com.yrdce.ipo.integrate;
 
-import gnnt.MEBS.common.mgr.model.Menu;
-import gnnt.MEBS.common.mgr.model.Right;
-import gnnt.MEBS.common.mgr.model.Role;
-import gnnt.MEBS.common.mgr.model.StandardModel;
-import gnnt.MEBS.common.mgr.model.TradeModule;
-import gnnt.MEBS.common.mgr.model.User;
-import gnnt.MEBS.logonService.dao.BaseDAOJdbc;
-
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,6 +15,14 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
 
+import gnnt.MEBS.common.mgr.model.Menu;
+import gnnt.MEBS.common.mgr.model.Right;
+import gnnt.MEBS.common.mgr.model.Role;
+import gnnt.MEBS.common.mgr.model.StandardModel;
+import gnnt.MEBS.common.mgr.model.TradeModule;
+import gnnt.MEBS.common.mgr.model.User;
+import gnnt.MEBS.logonService.dao.BaseDAOJdbc;
+
 public class QueryDao extends BaseDAOJdbc {
 
 	public String getFirmid(String arg) throws DataAccessException {
@@ -30,8 +30,7 @@ public class QueryDao extends BaseDAOJdbc {
 		String sql = "select t.firmid from M_TRADER t where t.traderid = ?";
 		Object[] o = { arg };
 		try {
-			currentPhase = (String) this.getJdbcTemplate().queryForObject(sql,
-					o, String.class);
+			currentPhase = (String) this.getJdbcTemplate().queryForObject(sql, o, String.class);
 		} catch (Exception e) {
 			currentPhase = "";
 			e.printStackTrace();
@@ -41,8 +40,7 @@ public class QueryDao extends BaseDAOJdbc {
 	}
 
 	public User getUserByID(String id) {
-		RowMapper<User> rm = ParameterizedBeanPropertyRowMapper
-				.newInstance(User.class);
+		RowMapper<User> rm = ParameterizedBeanPropertyRowMapper.newInstance(User.class);
 		String sql = "select * from C_USER t where t.id=?";
 		Object[] o = { id };
 		return this.getJdbcTemplate().queryForObject(sql, o, rm);
@@ -50,8 +48,7 @@ public class QueryDao extends BaseDAOJdbc {
 
 	public Set<Role> getAllRole() {
 		String sql = "select * from C_ROLE  ";
-		List<Map<String, Object>> roleList = this.getJdbcTemplate()
-				.queryForList(sql);
+		List<Map<String, Object>> roleList = this.getJdbcTemplate().queryForList(sql);
 		List<Role> roleInfos = new ArrayList<Role>();
 		for (Map<String, Object> map : roleList) {
 			Role roleInfo = new Role();
@@ -68,8 +65,7 @@ public class QueryDao extends BaseDAOJdbc {
 	public Set<Role> getUserRole(String id) {
 		String sql = "select r.* from c_user_role t left join c_role r on t.roleid=r.id where t.userid=?  ";
 		Object[] o = { id };
-		List<Map<String, Object>> roleList = this.getJdbcTemplate()
-				.queryForList(sql, o);
+		List<Map<String, Object>> roleList = this.getJdbcTemplate().queryForList(sql, o);
 		List<Role> roleInfos = new ArrayList<Role>();
 		for (Map<String, Object> map : roleList) {
 			Role roleInfo = new Role();
@@ -86,8 +82,7 @@ public class QueryDao extends BaseDAOJdbc {
 
 	public Set<Right> getAllRight() {
 		String sql = "select * from C_RIGHT where type!=-2 ";
-		List<Map<String, Object>> rightList = this.getJdbcTemplate()
-				.queryForList(sql);
+		List<Map<String, Object>> rightList = this.getJdbcTemplate().queryForList(sql);
 		List<Right> rightInfos = new ArrayList<Right>();
 		for (Map<String, Object> map : rightList) {
 			Right rightInfo = new Right();
@@ -99,12 +94,10 @@ public class QueryDao extends BaseDAOJdbc {
 			rightInfo.setUrl((String) map.get("url"));
 			rightInfo.setVisiturl((String) map.get("visiturl"));
 			if ((BigDecimal) map.get("moduleId") != null) {
-				rightInfo.setModuleId(((BigDecimal) map.get("moduleId"))
-						.intValue());
+				rightInfo.setModuleId(((BigDecimal) map.get("moduleId")).intValue());
 			}
 			if ((BigDecimal) map.get("visible") != null) {
-				rightInfo.setVisible(((BigDecimal) map.get("visible"))
-						.intValue());
+				rightInfo.setVisible(((BigDecimal) map.get("visible")).intValue());
 			}
 			if ((BigDecimal) map.get("seq") != null) {
 				rightInfo.setSeq(((BigDecimal) map.get("seq")).intValue());
@@ -122,8 +115,7 @@ public class QueryDao extends BaseDAOJdbc {
 	public Set<Right> getUserRight(String id) {
 		String sql = "select r.* from c_user_right t left join c_right r on t.rightid=r.id where t.userid=? and type!=-2 ";
 		Object[] o = { id };
-		List<Map<String, Object>> rightList = this.getJdbcTemplate()
-				.queryForList(sql, o);
+		List<Map<String, Object>> rightList = this.getJdbcTemplate().queryForList(sql, o);
 		List<Right> rightInfos = new ArrayList<Right>();
 		for (Map<String, Object> map : rightList) {
 			Right rightInfo = new Right();
@@ -135,12 +127,10 @@ public class QueryDao extends BaseDAOJdbc {
 			rightInfo.setUrl((String) map.get("url"));
 			rightInfo.setVisiturl((String) map.get("visiturl"));
 			if ((BigDecimal) map.get("moduleId") != null) {
-				rightInfo.setModuleId(((BigDecimal) map.get("moduleId"))
-						.intValue());
+				rightInfo.setModuleId(((BigDecimal) map.get("moduleId")).intValue());
 			}
 			if ((BigDecimal) map.get("visible") != null) {
-				rightInfo.setVisible(((BigDecimal) map.get("visible"))
-						.intValue());
+				rightInfo.setVisible(((BigDecimal) map.get("visible")).intValue());
 			}
 			if ((BigDecimal) map.get("seq") != null) {
 				rightInfo.setSeq(((BigDecimal) map.get("seq")).intValue());
@@ -167,14 +157,12 @@ public class QueryDao extends BaseDAOJdbc {
 
 	public List<StandardModel> getTradeModulesBySql() {
 		String sql = "select * from c_trademodule where 1=1";
-		List<Map<String, Object>> trademoduleList = this.getJdbcTemplate()
-				.queryForList(sql);
+		List<Map<String, Object>> trademoduleList = this.getJdbcTemplate().queryForList(sql);
 		List<StandardModel> rightInfos = new ArrayList<StandardModel>();
 		for (Map<String, Object> map : trademoduleList) {
 			TradeModule rightInfo = new TradeModule();
 			if ((BigDecimal) map.get("moduleId") != null) {
-				rightInfo.setModuleId(((BigDecimal) map.get("moduleId"))
-						.intValue());
+				rightInfo.setModuleId(((BigDecimal) map.get("moduleId")).intValue());
 			}
 			rightInfo.setAddFirmFn((String) map.get("addFirmFn"));
 			rightInfo.setCnName((String) map.get("cnName"));
@@ -183,47 +171,42 @@ public class QueryDao extends BaseDAOJdbc {
 			rightInfo.setIsFirmSet((String) map.get("isFirmSet"));
 			rightInfo.setIsNeedBreed((String) map.get("isNeedBreed"));
 			rightInfo.setShortName((String) map.get("shortName"));
-			rightInfo.setUpdateFirmStatusFn((String) map
-					.get("updateFirmStatusFn"));
+			rightInfo.setUpdateFirmStatusFn((String) map.get("updateFirmStatusFn"));
 			rightInfos.add(rightInfo);
 		}
 		return rightInfos;
 	}
 
-	public Menu getMenuById(long paramLong, int paramInt1, int paramInt2,
-			int paramInt3, List<Integer> paramList) {
+	public Menu getMenuById(long paramLong, int paramInt1, int paramInt2, int paramInt3, List<Integer> paramList) {
 		try {
-			String sql = "select * from C_RIGHT  where ID = :paramLong and  PARENTID != -1  and (TYPE = :paramInt1 or TYPE = :paramInt2) and VISIBLE =:paramInt3 and MODULEID in(:param) order by SEQ";
+			String sql = "select * from C_RIGHT  where ID = :paramLong and  (TYPE = :paramInt1 or TYPE = :paramInt2) and VISIBLE =:paramInt3 and MODULEID in(:param) order by SEQ";
 			Map<String, Object> paramMap = new HashMap<String, Object>();
 			paramMap.put("paramLong", paramLong);
 			paramMap.put("paramInt1", paramInt1);
 			paramMap.put("paramInt2", paramInt2);
 			paramMap.put("paramInt3", paramInt3);
 			paramMap.put("param", paramList);
-			NamedParameterJdbcTemplate jdbc = new NamedParameterJdbcTemplate(
-					this.getJdbcTemplate());
-			List<Menu> menulist = jdbc.query(sql, paramMap,
-					new RowMapper<Menu>() {
+			NamedParameterJdbcTemplate jdbc = new NamedParameterJdbcTemplate(this.getJdbcTemplate());
+			List<Menu> menulist = jdbc.query(sql, paramMap, new RowMapper<Menu>() {
 
-						@Override
-						public Menu mapRow(ResultSet rs, int index)
-								throws SQLException {
-							Menu rightInfo = new Menu();
-							rightInfo.setId(rs.getLong("id"));
-							rightInfo.setName(rs.getString("name"));
-							rightInfo.setIcon(rs.getString("icon"));
-							rightInfo.setUrl(rs.getString("url"));
-							rightInfo.setModuleId(rs.getInt("moduleId"));
-							rightInfo.setVisible(rs.getInt("visible"));
-							rightInfo.setSeq(rs.getInt("seq"));
-							rightInfo.setParentID(rs.getLong("parentID"));
-							return rightInfo;
-						}
-					});
+				@Override
+				public Menu mapRow(ResultSet rs, int index) throws SQLException {
+					Menu rightInfo = new Menu();
+					rightInfo.setId(rs.getLong("id"));
+					rightInfo.setName(rs.getString("name"));
+					rightInfo.setIcon(rs.getString("icon"));
+					rightInfo.setUrl(rs.getString("url"));
+					rightInfo.setModuleId(rs.getInt("moduleId"));
+					rightInfo.setVisible(rs.getInt("visible"));
+					rightInfo.setSeq(rs.getInt("seq"));
+					rightInfo.setParentID(rs.getLong("parentID"));
+					return rightInfo;
+				}
+			});
 			if (menulist.size() > 0) {
 				return menulist.get(0);
 			}
-			return null;
+			return new Menu();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
